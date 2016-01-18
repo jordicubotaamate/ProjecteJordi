720075d2a0055736bf721ac586a84e61ff1a508a
Jordi Cubota Amate <cubota.jordi.inf2@gmail.com>
2016-01-18 19:50:05 +0100

    First Finances DB

---
+++ app/src/main/java/cat/infoserveis/jordi/projectejordi/BasesDeDades/FinancesDataBaseAdapter.java
@@ -0,0 +1,90 @@
+package cat.infoserveis.jordi.projectejordi.BasesDeDades;
+
+/**
+ * Created by Jordi on 05/01/2016.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class FinancesDataBaseAdapter
+{
+    static final String DATABASE_NAME = "Finances.db";
+    static final int DATABASE_VERSION = 1;
+    public static final int NAME_COLUMN = 1;
+    // TODO: Create public field for each column in your table.
+    // SQL Statement to create a new database.
+    static final String DATABASE_CREATE = "create table "+"FINANCES"+
+            "( " +"ID"+" integer primary key autoincrement,"+ "ID-OWNER  integer, AMOUNT float, DATE DATE, TOTAL-MONEY float); ";
+    // Variable to hold the database instance*/
+    public  SQLiteDatabase db;
+    // Context of the application using the database.
+    private final Context context;
+    // Database open/upgrade helper
+    private DataBaseHelper dbHelper;
+    public FinancesDataBaseAdapter(Context _context)
+    {
+        context = _context;
+        dbHelper = new DataBaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public FinancesDataBaseAdapter open() throws SQLException
+    {
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+    public void close()
+    {
+        db.close();
+    }
+
+    public  SQLiteDatabase getDatabaseInstance()
+    {
+        return db;
+    }
+
+    public void insertEntry(String userName,String password)
+    {
+        ContentValues newValues = new ContentValues();
+        // Assign values for each row.
+        newValues.put("USERNAME", userName);
+        newValues.put("PASSWORD",password);
+
+        // Insert the row into your table
+        db.insert("LOGIN", null, newValues);
+        ///Toast.makeText(context, "Reminder Is Successfully Saved", Toast.LENGTH_LONG).show();
+    }
+    public int deleteEntry(String UserName)
+    {
+        //String id=String.valueOf(ID);
+        String where="USERNAME=?";
+        int numberOFEntriesDeleted= db.delete("LOGIN", where, new String[]{UserName}) ;
+        // Toast.makeText(context, "Number fo Entry Deleted Successfully : "+numberOFEntriesDeleted, Toast.LENGTH_LONG).show();
+        return numberOFEntriesDeleted;
+    }
+    public String getSinlgeEntry(String userName)
+    {
+        Cursor cursor=db.query("LOGIN", null, " USERNAME=?", new String[]{userName}, null, null, null);
+        if(cursor.getCount()<1) // UserName Not Exist
+        {
+            cursor.close();
+            return "";//Aixi podem utilitzar aquesta funcio per saber si l'usuari existeix o no.
+        }
+        cursor.moveToFirst();
+        String password= cursor.getString(cursor.getColumnIndex("PASSWORD"));
+        cursor.close();
+        return password;
+    }
+    public void  updateEntry(String userName,String password)
+    {
+        // Define the updated row content.
+        ContentValues updatedValues = new ContentValues();
+        // Assign values for each row.
+        updatedValues.put("USERNAME", userName);
+        updatedValues.put("PASSWORD",password);
+
+        String where="USERNAME = ?";
+        db.update("LOGIN",updatedValues, where, new String[]{userName});
+    }
+}
